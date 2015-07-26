d693af743555ece7b87479573a2fd8697eb60b13
taptap <alexey.e.egorov@gmail.com>
2015-07-26 11:45:47 +0300

    finish hello world

--- hello/hello.asm
+++ hello/hello.asm
@@ -1,11 +1,28 @@
-global main
-extern printf
-
 section .data
-text: db "Hello, world!", 0
+text:   db "Hello, %s!", 10, 0
+
+section .bss
+name:   resb 64
 
 section .text
+
+global main
+extern printf
+extern fgets
+extern stdin
+
 main:
+        mov  rdi, name
+        mov  rsi, 64
+        mov  rdx, [stdin]
+        mov  rax, 0
+        call fgets
+
         mov rdi, text
+        mov rsi, name
         mov rax, 0
         call printf
+
+        mov rax, 60
+        mov rdi, 0
+        syscall
