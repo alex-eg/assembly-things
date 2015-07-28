1f077d13ea75bfb52a8b042504d737810fbe4dcd
taptap <alexey.e.egorov@gmail.com>
2015-07-28 18:52:10 +0300

    fix newline problems, add comments

--- print-argv-syscalls/main.asm
+++ print-argv-syscalls/main.asm
@@ -8,8 +8,8 @@ next_argv:
         pop r9                  ; argv[i]
         mov rsi, r9
 get_size_loop:
-        lodsb
-        cmp rax, 0
+        lodsb                   ; load byte from memory pointed by rsi to rax, and increase rsi by 1 byte
+        cmp rax, 0              ; strings are null-terminated
         jne increase
 print:
         mov rax, 1
@@ -18,16 +18,17 @@ print:
         mov rdx, r8
         syscall
 
-        dec rbx
-        cmp rbx, 0
-        jg  next_argv
-
-        push 10
+        push 10                 ; print newline
         mov rax, 1
         mov rdi, 1
         mov rsi, rsp
         mov rdx, 1
         syscall
+        pop rax
+
+        dec rbx
+        cmp rbx, 0
+        jg  next_argv
 
         mov rax, 60
         mov rsi, 0
@@ -35,3 +36,4 @@ print:
 increase:
         inc r8
         jmp get_size_loop
+
