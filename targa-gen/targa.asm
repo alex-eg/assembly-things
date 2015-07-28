d50a20efec3a477c742fb9e439f762f271287d2e
taptap <alexey.e.egorov@gmail.com>
2015-07-29 02:28:15 +0300

    add function for writing targa files

--- targa-gen/targa.asm
+++ targa-gen/targa.asm
@@ -3,39 +3,63 @@
 ;;; rax is return register
 ;;; rbp, rbx, r12, r13, r14, r15 must be preserved between calls
 
+%macro write_data 3             ; file, buffer, count
+
+        mov rax, 1
+        mov rdi, %1
+        mov rsi, %2
+        mov rdx, %3
+        syscall
+
+%endmacro
+
 section .data
-signature: db "TRUEVISION-XFILE.", 0
-usage: db "usage: targa-gen <dest-file>", 10, 0
+
+header:   db 0,0,2,0,0,0,0,0
+
+x_origin: db 0,0
+y_origin: db 0,0
+depth:    db 24
+image_descriptor: db 00_00_0000b
+
+footer: db 0,0,0,0,0,0,0,0, "TRUEVISION-XFILE.", 0
 
 section .text
 
-global _start
+global write_targa
 
-extern print
-extern chunck_length
-
-_start:
-        pop rbx                 ; argc
-        call check_argc
-        mov rdi, 0
-        jmp exit
-
-check_argc:
+write_targa:
+;; rdi - opened for writing file
+;; rsi - width pixels
+;; rdx - height pixels
+;; rcx - pointer to pixels
         enter 0, 0
-        cmp rbx, 1
-        jne .print_usage_and_exit
+
+        mov r12, rdi
+        mov r13, rsi
+        mov r14, rdx
+        mov r15, rcx
+
+        write_data r12, header,   8
+        write_data r12, x_origin, 2
+        write_data r12, y_origin, 2
+        push r13
+        write_data r12, rsp,      2
+        push r14
+        write_data r12, rsp,      2
+        write_data r12, depth,    1
+        write_data r12, image_descriptor, 1
+
+        mov rax, r13
+b1:
+        mul r14
+        mov r14, 3
+        mul r14
+b2:
+        mov rbx, rax
+b3:
+        write_data r12, r15, rbx
+        write_data r12, footer, 26
+
         leave
         ret
-.print_usage_and_exit:
-        mov rdi, usage
-        mov rsi, 0
-        call print
-
-        mov rdi, 1
-        jmp exit
-exit:
-        mov rax, 60
-        mov rsi, rdi
-        syscall
-
-        
