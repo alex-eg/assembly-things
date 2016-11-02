7be7f8916a80b6ced0c165a42f4815891756eeda
taptap <alexey.e.egorov@gmail.com>
2016-11-02 18:44:07 +0300

    more wip
    now on ymm registers and 256-bit packs

--- floating-point/fp.asm
+++ floating-point/fp.asm
@@ -1,7 +1,8 @@
 section .data
-a:      dq 0.5
-b:      dq 0.4
-fmt:    db "Result: %s\n", 0
+	align 32
+a:      dq 0.5, 0.6, 0.7, 0.8
+b:      dq 0.4, 0.3, 0.2, 0.1
+fmt:    db "Result: %.9f %.9f %.9f %.9f",10,0
 
 section .text
 
@@ -9,16 +10,21 @@ global main
 extern printf
 
 main:
-        ;fld qword [a]
-        ;fld qword [b]
-        ;fsubp st1, st0
-        ;fst qword [a]
+	push rbp
+	mov rbp, rsp
+	sub rsp, 32
+
+	vmovapd ymm0, [a]
+	vsubpd ymm0, [b]
+	vmovapd [rsp], ymm0
 
         mov rdi, fmt
-        mov rsi, 22
-	mov rax, 0
+	mov rax, 1
         call printf
 
-        mov rax, 60
+	mov rsp, rbp
+	pop rbp
+
+	mov rax, 60
         mov rdi, 0
         syscall
